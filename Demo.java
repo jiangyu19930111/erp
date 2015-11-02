649421d64cedbef07c10eb9ba5e6f3aef05e67d2
mldn <mldnqa@163.com>
2015-11-02 22:34:03 +0800

    Add Two Files

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Hello{
+	public static void mian(String orgs[]){
+		System.out.println("Hello Word !!");
+		System.out.println("ÄãºÃ£¡");
+	}
+}
---
+++ Emp.java
@@ -0,0 +1,7 @@
+public class Hello{
+	public static void mian(String orgs[]){
+		System.out.println("你好!!");
+		System.out.println("我好!!");
+		System.out.println("大家好!!");
+	}
+}
